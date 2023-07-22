e47fd2aaf9f8c880e1644f4d905eb93cf5ca7987
Богдан Ковальский <kovalskii.bi@students.dvfu.ru>
2023-07-22 21:09:55 +0800

    Подключение к базе данных apidata.mos.ru с использованием api-ключа

--- FindingApplication.py
+++ FindingApplication.py
@@ -1,1 +1,10 @@
-print ('Hello')
+import requests
+x = input()
+s = "https://apidata.mos.ru/v1/datasets/658/?api_key=a47e7dc029852f8972d61616d5cf5d36"
+#s += x
+response = requests.get(s)
+print(response)
+if response.status_code == 200:
+    print(requests.get(s))
+    print(response.text)
+    print(response.url)
