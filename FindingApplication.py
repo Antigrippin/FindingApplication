1ce8e0d3869ad3d95b36e0bec20a98d4c7badf2b
Богдан Ковальский <kovalskii.bi@students.dvfu.ru>
2023-07-22 23:02:34 +0800

    Добавление возможности просмотра определенных баз данных по вводу их ID

--- FindingApplication.py
+++ FindingApplication.py
@@ -1,9 +1,17 @@
 # -*- coding: cp1251 -*-
 import requests
+
+def delete_api(req):
+    answ = req[:-42]
+    return answ
+
 good_key = ["Id", "SefUrl", "CategoryCaption", "Caption"]
-s = "https://apidata.mos.ru/v1/datasets/"
-s += "/?api_key=a47e7dc029852f8972d61616d5cf5d36"
-response = requests.post(s) #requests
+bad_key = ["Number", "global_id"]
+orig_link = "https://apidata.mos.ru/v1/datasets/"
+api_key = "/?api_key=a47e7dc029852f8972d61616d5cf5d36"
+print(len(api_key))
+req = orig_link+api_key
+response = requests.post(req) #requests
 print(response)
 if response.status_code == 200:
     print("Successfull connect")
@@ -14,6 +22,42 @@ if response.status_code == 200:
             if key in good_key:
                 print(key, ":", value)
         print()
-#    print(r)
- #   print(response.text)
     print(response.url) # link to website
+
+
+req = delete_api(req)
+print('Req now : ', req)
+print('Введите ID для перехода:')
+req_id = input()
+
+
+req += req_id + api_key
+response = requests.post(req)
+if response.status_code == 200:
+    print("Successfull connect")
+    print(response.text)
+  # r = response.json() #create a dictionary
+   # print(r)
+ #   for one_res in r: 
+ #       for key,value in one_res.items():
+  #          if key in good_key:
+ #               print(key, ":", value)
+ #       print()
+    print(response.url) # link to website
+
+print('Таблица: ', end = '\n\n\n\n')
+req = delete_api(req)
+req += "/rows" + api_key
+print('\nreq = ', req, end = '\n\n')
+response = requests.post(req)
+if response.status_code == 200:
+    r = response.json()
+    for one_req in r:
+        print(one_req)
+        break;
+    for one_req in r:
+        for key,value in one_req.items():
+            if (key in bad_key) == False:
+                print(value)
+else:
+    print("Error")
