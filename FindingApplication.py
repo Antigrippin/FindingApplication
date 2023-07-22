2a9c4691e7ec4a78bc0adca4ada28beae5a3a873
Богдан Ковальский <kovalskii.bi@students.dvfu.ru>
2023-07-22 22:10:54 +0800

    Добавление возможности просмотра всех баз данных с выводом их Id, Url и названия

--- FindingApplication.py
+++ FindingApplication.py
@@ -1,10 +1,23 @@
+# -*- coding: cp1251 -*-
 import requests
+good_key = ["Id", "SefUrl", "CategoryCaption", "Caption"]
 x = input()
-s = "https://apidata.mos.ru/v1/datasets/658/?api_key=a47e7dc029852f8972d61616d5cf5d36"
-#s += x
-response = requests.get(s)
+print(x)
+s = "https://apidata.mos.ru/v1/datasets/"
+if len(x) != 0:
+    s += x
+s += "/?api_key=a47e7dc029852f8972d61616d5cf5d36"
+response = requests.post(s) #запрос
 print(response)
 if response.status_code == 200:
-    print(requests.get(s))
-    print(response.text)
-    print(response.url)
+    print("Successfull connect")
+    r = response.json() #создание словаря
+ #   print(r)
+    for one_res in r: 
+        for key,value in one_res.items():
+            if key in good_key:
+                print(key, ":", value)
+        print()
+#    print(r)
+ #   print(response.text)
+    print(response.url) # вывод ссылки на данные
