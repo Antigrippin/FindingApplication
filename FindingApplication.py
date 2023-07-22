7678e74444a7372512ce6735510f22d9aa7e7dfe
Богдан Ковальский <kovalskii.bi@students.dvfu.ru>
2023-07-22 22:13:03 +0800

    Исправление комментариев на английский язык

--- FindingApplication.py
+++ FindingApplication.py
@@ -1,17 +1,13 @@
 # -*- coding: cp1251 -*-
 import requests
 good_key = ["Id", "SefUrl", "CategoryCaption", "Caption"]
-x = input()
-print(x)
 s = "https://apidata.mos.ru/v1/datasets/"
-if len(x) != 0:
-    s += x
 s += "/?api_key=a47e7dc029852f8972d61616d5cf5d36"
-response = requests.post(s) #запрос
+response = requests.post(s) #requests
 print(response)
 if response.status_code == 200:
     print("Successfull connect")
-    r = response.json() #создание словаря
+    r = response.json() #create a dictionary
  #   print(r)
     for one_res in r: 
         for key,value in one_res.items():
@@ -20,4 +16,4 @@ if response.status_code == 200:
         print()
 #    print(r)
  #   print(response.text)
-    print(response.url) # вывод ссылки на данные
+    print(response.url) # link to website
